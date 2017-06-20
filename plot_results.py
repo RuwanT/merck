902239023727e03dceb73c6039c418e8ec459a3e
RuwanT <ruwant.email@gmail.com>
2017-06-20 15:15:52 +1000

    ploting results for feature selection
    ploting results for feature selection

--- plot_results.py
+++ plot_results.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
+from nutsflow import *
+from nutsml import *
 
 # Global variables
 BATCH_SIZE = 64
@@ -12,11 +14,11 @@ MAX_GENERATIONS = 10
 PLOT_FEATURE_IMP = False
 PLOT_ACCURACY_GEN = True
 
-
 data_root = '/home/truwan/DATA/merck/preprocessed/'
 
 dataset_names = ['CB1', 'DPP4', 'HIVINT', 'HIVPROT', 'METAB', 'NK1', 'OX1', 'PGP', 'PPB', 'RAT_F',
                  'TDI', 'THROMBIN', 'OX2']
+
 
 # dataset_names = ['CB1', 'LOGD']
 
@@ -24,7 +26,9 @@ dataset_names = ['CB1', 'DPP4', 'HIVINT', 'HIVPROT', 'METAB', 'NK1', 'OX1', 'PGP
 def plot_net_evolve():
     for dataset_name in dataset_names:
         read_header = ['gen_str', 'type', 'run', 'mean', 'std', 'med']
-        results_frame = pd.read_csv("C:\\GIT_codes\\backup_models\\outputs_train_evo_net\\test_errors_" + dataset_name + '.csv', header=None, names=read_header)
+        results_frame = pd.read_csv(
+            "C:\\GIT_codes\\backup_models\\outputs_train_evo_net\\test_errors_" + dataset_name + '.csv', header=None,
+            names=read_header)
 
         fig, ax1 = plt.subplots()
         ax2 = ax1.twinx()
@@ -48,19 +52,19 @@ def plot_net_evolve():
             # plt.pause(0.1)
             gen += 1
 
-        ax2.plot(range(1, gen+1), gen_nparam, 'ro-')
+        ax2.plot(range(1, gen + 1), gen_nparam, 'ro-')
         plt.pause(0.1)
 
-        ax1.set_ylim([0, 2*mean_0])
+        ax1.set_ylim([0, 2 * mean_0])
         ax1.set_xlabel('Generation')
         ax1.set_ylabel('RMSE', color='b')
         ax1.tick_params('y', colors='b')
-        print gen_nparam, np.percentile(gen_nparam,q=60)
-        ax2.set_ylim([0, np.percentile(gen_nparam,q=60)])
+        print gen_nparam, np.percentile(gen_nparam, q=60)
+        ax2.set_ylim([0, np.percentile(gen_nparam, q=60)])
         ax2.set_ylabel('Network Efficiency', color='r')
         ax2.tick_params('y', colors='r')
 
-        ax1.axhline(y=mean_0, xmin=0.0, xmax=gen-1, linewidth=1, color='k', linestyle='dashed')
+        ax1.axhline(y=mean_0, xmin=0.0, xmax=gen - 1, linewidth=1, color='k', linestyle='dashed')
 
         fig.tight_layout()
 
@@ -79,28 +83,146 @@ def print_res_of_gen(gen):
 
         gen_nparam = list()
         for index, row in gen_results.iterrows():
-            if str(gen) in  row['gen_str']:
+            if str(gen) in row['gen_str']:
                 print dataset_name, row['med'], row['nparam']
 
 
-def selected_features():
-    a = np.load('/home/truwan/projects/merck/outputs/featureSelect_CB1_0_8.npy')
-    b = np.load('/home/truwan/projects/merck/outputs/featureSelect_CB1_1_8.npy')
-    # print len(a)
-    # print np.nonzero(a)[0].shape
-    a = set(np.nonzero(a)[0])
-    b = set(np.nonzero(b)[0])
+def selected_feature_overalp():
+    # Use average correlation to compare the features selected in different runs of the neural network
+    GEN = 9
+    NNRUN = 5
+    np.set_printoptions(precision=3)
+    res = list()
+    for dataset_name in dataset_names:
 
-    print a.union(b)
-    print a.intersection(b)
+        train_file = data_root + dataset_name + '_training_disguised.csv'
+        assert os.path.isfile(train_file)
+        data_test = ReadPandas(train_file, dropnan=True)
+        data_frm = data_test.dataframe.ix[:, 2:]
+        data_corr = data_frm.corr().values
 
-    ab =a.intersection(b)
-    apb = a.union(b)
+        read_header = ['nrun', 'gen', 'test_RMSE', 'test_R2', 'val_RMSE', 'num_features']
+        results_frame = pd.read_csv(
+            "./outputs/feature_selection_bm_" + dataset_name + '.csv', header=None, names=read_header)
+        temp = dict()
+        for nrun in range(0, NNRUN):
+            feature_npy_name = './outputs/featureSelect_bm_' + dataset_name + '_' + str(nrun) + '_' + str(GEN) + '.npy'
+            if os.path.isfile(feature_npy_name):
+                temp[nrun] = list(np.nonzero(np.load(feature_npy_name))[0])
 
-    print float(len(ab)) / float(len(apb))
+        out = np.zeros((NNRUN, NNRUN), dtype=float)
+        for nrun_i, sel_features_i in temp.iteritems():
+            for nrun_j, sel_features_j in temp.iteritems():
+                if nrun_j <= nrun_i:
+                    continue
+
+                out[nrun_i, nrun_j] = np.mean(np.max((data_corr[sel_features_i, :])[:, sel_features_j], axis=1)) * 100
+                print dataset_name, nrun_j, nrun_i, out[nrun_i, nrun_j], \
+                    results_frame.loc[results_frame['nrun'] == nrun_i]['test_RMSE'].values[GEN], \
+                    results_frame.loc[results_frame['nrun'] == nrun_j]['test_RMSE'].values[GEN], len(
+                    sel_features_i), len(sel_features_j)
+
+                res.append([dataset_name, nrun_j, nrun_i, out[nrun_i, nrun_j],
+                            results_frame.loc[results_frame['nrun'] == nrun_i]['test_RMSE'].values[GEN],
+                            results_frame.loc[results_frame['nrun'] == nrun_j]['test_RMSE'].values[GEN],
+                            len(sel_features_i), len(sel_features_j)])
+
+        print out
+
+    writer = WriteCSV('./outputs/feature_overlap.csv')
+    res >> writer
+
+
+def plot_feature_selection():
+    for dataset_name in dataset_names:
+
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twinx()
+
+        read_header = ['nrun', 'gen', 'test_RMSE', 'test_R2', 'val_RMSE', 'num_features']
+        results_frame = pd.read_csv(
+            "./outputs/feature_selection_bm_" + dataset_name + '.csv', header=None, names=read_header)
+
+        print 'Plotting dataset ' + dataset_name
+
+        run_rmse = list()
+        nfeatures = list()
+        for nrun in range(0, 5):
+            run_rmse.append(results_frame.loc[results_frame['nrun'] == nrun]['test_RMSE'].values)
+            nfeatures.append(results_frame.loc[results_frame['nrun'] == nrun]['num_features'].values)
+
+        run_rmse = (np.asarray(run_rmse))
+
+        ax1.boxplot(run_rmse)
+        ax2.plot(range(1, 10 + 1), np.mean(np.asarray(nfeatures), axis=0, keepdims=False), 'ro-')
+
+        ax1.set_ylim([0, np.max(run_rmse) * 1.1])
+        ax1.set_xlabel('Generation')
+        ax1.set_ylabel('RMSE', color='b')
+        ax1.tick_params('y', colors='b')
+        ax2.set_ylim([0, np.max(nfeatures) * 1.1])
+        ax2.set_ylabel('Number of Features', color='r')
+        ax2.tick_params('y', colors='r')
+
+        plt.pause(.1)
+
+        fig.tight_layout()
+
+        plt.savefig('./outputs/feature_select_' + dataset_name + '.tiff')
+        plt.close()
+
+
+def plot_corr():
+    import scipy
+    import pylab
+    import scipy.cluster.hierarchy as sch
+
+    dataset_name = 'CB1'
+    test_file = data_root + dataset_name + '_test_disguised.csv'
+    data_test = ReadPandas(test_file, dropnan=True)
+    data_frm = data_test.dataframe.ix[:, 2:]
+    # data_frm = np.abs(data_frm.corr().values)
+
+    import seaborn as sns
+    sns.set()
+
+    g = sns.clustermap(data_frm.corr())
+    g.savefig("a.png")
+    # # Compute and plot dendrogram.
+    # fig = pylab.figure()
+    # axdendro = fig.add_axes([0.09, 0.1, 0.2, 0.8])
+    # Y = sch.linkage(data_frm, method='centroid')
+    # Z = sch.dendrogram(Y, orientation='right')
+    # axdendro.set_xticks([])
+    # axdendro.set_yticks([])
+    #
+    # # Plot distance matrix.
+    # axmatrix = fig.add_axes([0.3, 0.1, 0.6, 0.8])
+    # index = Z['leaves']
+    # D = data_frm[index, :]
+    # D = D[:, index]
+    # im = axmatrix.matshow(D, aspect='auto', origin='lower')
+    # axmatrix.set_xticks([])
+    # axmatrix.set_yticks([])
+    #
+    # # Plot colorbar.
+    # axcolor = fig.add_axes([0.91, 0.1, 0.02, 0.8])
+    # pylab.colorbar(im, cax=axcolor)
+    #
+    # # Display and save figure.
+    # fig.show()
+    # fig.savefig('a.png')
+
+
+    # plt.imshow(data_frm, interpolation="nearest", cmap='jet')
+    # plt.colorbar()
+    # plt.show()
 
 
 if __name__ == "__main__":
-    plot_net_evolve()
+    # plot_net_evolve()
     # print_res_of_gen(0)
     # selected_features()
+    # plot_feature_selection()
+    selected_feature_overalp()
+    # plot_corr()
